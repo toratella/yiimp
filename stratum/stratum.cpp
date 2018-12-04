f9ac19b64ae58da012eb73032d6ba8d6bc85eb44
toratella <44305799+toratella@users.noreply.github.com>
2018-12-04 22:31:11 +0000

    Update stratum.cpp

--- stratum/stratum.cpp
+++ stratum/stratum.cpp
@@ -134,6 +134,13 @@ YAAMP_ALGO g_algos[] =
 	{"penta", penta_hash, 1, 0, 0},
 	{"skein2", skein2_hash, 1, 0, 0},
 	{"yescrypt", yescrypt_hash, 0x10000, 0, 0},
+	{"yescryptR16", yescryptR16_hash, 0x10000, 0, 0 },
+	{"yescryptR32", yescryptR32_hash, 0x10000, 0, 0 },
+	{"yespower", yespower_hash, 0x10000, 0, 0 },
+	{"yespowerR8", yespowerR8_hash, 0x10000, 0, 0 },
+	{"yespowerR16", yespowerR16_hash, 0x10000, 0, 0 },
+	{"yespowerR24", yespowerR24_hash, 0x10000, 0, 0 },
+	{"yespowerR32", yespowerR32_hash, 0x10000, 0, 0 },
 	{"zr5", zr5_hash, 1, 0, 0},
 
 	{"hive", hive_hash, 0x10000, 0, 0},
